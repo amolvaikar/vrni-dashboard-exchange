71a839072e04fb68677f962bb1f5e628c117d71b
avaikar <avaikar@vmware.com>
2023-06-09 23:17:53 +0200

    Added ability to parameterise json input files and added json for application's network metrics

--- create-dashboard.py
+++ create-dashboard.py
@@ -4,10 +4,14 @@ import json
 from optparse import OptionParser
 import urllib3
 from types import SimpleNamespace
+import re
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
+###############################################################
+################ vRNI Session Management ######################
+###############################################################
 def open_vrni_public_api_session(url, user_id, password):
     try:
         session = requests.Session()
@@ -87,7 +91,10 @@ def open_vrni_private_api_session(url, user_id, password):
     return None
 
 
-def create_pinboard_pre_69(session, pinboard_name, pinboard_description):
+###############################################################
+################ Pinboard and Pin creation ####################
+###############################################################
+def create_pinboard_pre_db(session, pinboard_name, pinboard_description):
     body = '''{{"name": "{0}", "description": "{1}"}}'''.format(pinboard_name, pinboard_description)
     response = session.post(url + "/api/ni/pinboards", data=body, verify=False)
     if response.status_code == 201:
@@ -98,7 +105,7 @@ def create_pinboard_pre_69(session, pinboard_name, pinboard_description):
     return None
 
 
-def create_pinboard_post_69(session, dashboard_name, dashboard_description):
+def create_pinboard_post_db(session, dashboard_name, dashboard_description):
     body = '''{{"name": "{0}", "description": "{1}"}}'''.format(dashboard_name, dashboard_description)
     response = session.post(url + "/api/custom-dashboards", data=body, verify=False)
     if response.status_code == 201:
@@ -109,7 +116,7 @@ def create_pinboard_post_69(session, dashboard_name, dashboard_description):
     return None
 
 
-def add_pin_to_pinboard_pre_69(session, pinboard_id, pin_name, pin_query):
+def add_pin_to_pinboard_pre_db(session, pinboard_id, pin_name, pin_query):
     body = '''{{"name": "{0}", "query": "{1}"}}'''.format(pin_name, pin_query)
     response = session.post(url + "/api/ni/pinboards/{}/pins".format(pinboard_id), data=body, verify=False)
     if response.status_code != 201:
@@ -118,7 +125,7 @@ def add_pin_to_pinboard_pre_69(session, pinboard_id, pin_name, pin_query):
     return
 
 
-def add_pin_to_pinboard_post_69(session, dashboard_id, pin_name, pin_query):
+def add_pin_to_pinboard_post_db(session, dashboard_id, pin_name, pin_query):
     body = '{"id":"' + pin_name + '","query":"' + pin_query + '", "isApplet": false, "dataBlob": "{}", "entities":[]}'
     print(body)
     response = session.post(url + "/api/custom-dashboards/{}/pins".format(dashboard_id), data=body, verify=False)
@@ -127,23 +134,14 @@ def add_pin_to_pinboard_post_69(session, dashboard_id, pin_name, pin_query):
     return
 
 
+###############################################################
+################ Data & Param validation ######################
+###############################################################
 def check_options(options):
     if options.dashboard_file is not None and options.online_dashboard_file is not None:
         print("Cant use online as well as offline file at same time")
         return False
     return True
-
-
-def read_dashboard_json(dashboard_file):
-    with open(dashboard_file, "r") as input_file:
-        json_text = input_file.read()
-
-    dashboard_json_object = None
-    try:
-        dashboard_json_object = json.loads(json_text, object_hook=lambda d: SimpleNamespace(**d))
-    except:
-        print("Failed while loading the json from " + dashboard_file)
-    return dashboard_json_object
 
 
 def is_valid_dashboard_json(json_object, use_public_api):
@@ -162,8 +160,9 @@ def is_valid_dashboard_json(json_object, use_public_api):
             return json_object_is_valid
 
         if len(json_object.pins) > 20 and use_public_api:
-            print("WARNING: There are more than 20 pins in this definition, but vRNI version you are using allows only 20 pins per pinboard. "
-                  "This script will use only the first 20 pins from the file")
+            print(
+                "WARNING: There are more than 20 pins in this definition, but vRNI version you are using allows only 20 pins per pinboard. "
+                "This script will use only the first 20 pins from the file")
 
         for pin in json_object.pins:
             if not hasattr(pin, "pin_name") or not hasattr(pin, "pin_description") or not hasattr(pin, "pin_query"):
@@ -177,6 +176,51 @@ def is_valid_dashboard_json(json_object, use_public_api):
     return json_object_is_valid
 
 
+def check_if_args_available(string_to_be_validated, dict_of_available_args):
+    list_of_required_args = get_all_args_from_string(string_to_be_validated)
+    for req_arg in list_of_required_args:
+        if dict_of_available_args.get(req_arg.replace("{", "").replace("}", "")) is None:
+            return False
+    return True
+
+
+def json_has_valid_args(dashboard_json_object, dashboard_args_dict):
+    if not check_if_args_available(dashboard_json_object.default_board_name, dashboard_args_dict):
+        print("Args missing for dashboard name")
+        return False
+    if not check_if_args_available(dashboard_json_object.description, dashboard_args_dict):
+        print("Args missing for description")
+        return False
+    for pin in dashboard_json_object.pins:
+        if not check_if_args_available(pin.pin_query, dashboard_args_dict):
+            print("Args missing for pin query " + pin.pin_name)
+            return False
+    return True
+
+
+def substitute_arguments(strings_with_args, dashboard_args_dict):
+    for key in dashboard_args_dict.keys():
+        strings_with_args = strings_with_args.replace("{"+key+"}", dashboard_args_dict.get(key))
+    return strings_with_args
+
+###############################################################
+################ Misc. Utils ##################################
+###############################################################
+def build_dashboard_args_dict(dashboard_arguments):
+    dashboard_args_dict = dict()
+    if dashboard_arguments is None:
+        return dashboard_args_dict
+    dbargs_list = dashboard_arguments.split(",")
+    for dbarg in dbargs_list:
+        k, v = dbarg.split('=')
+        dashboard_args_dict[k] = v
+    return dashboard_args_dict
+
+
+def get_all_args_from_string(string_with_args):
+    return re.findall(r'{.*?}', string_with_args)
+
+
 def find_vrni_version(url, user_id, password):
     session = open_vrni_public_api_session(url, user_id, password)
     response = session.get(url + "/api/ni/info/version", verify=False)
@@ -188,7 +232,35 @@ def find_vrni_version(url, user_id, password):
     return vrni_version
 
 
-def create_dashboard(url, user_id, password, dashboard_json_object, dashboard_name, use_public_api):
+def download_dashboard_file(sourceurl, destinationfile="./temp_dashboard.json"):
+    response = requests.get(sourceurl)
+    if response.status_code == 200:
+        with open(destinationfile, 'wb') as file:
+            file.write(response.content)
+        print("Online Dashboard File downloaded successfully.")
+    else:
+        print("Failed to download the online Dashboard file.")
+        return None
+    return destinationfile
+
+
+def read_dashboard_json(dashboard_file):
+    with open(dashboard_file, "r") as input_file:
+        json_text = input_file.read()
+
+    dashboard_json_object = None
+    try:
+        dashboard_json_object = json.loads(json_text, object_hook=lambda d: SimpleNamespace(**d))
+    except:
+        print("Failed while loading the json from " + dashboard_file)
+    return dashboard_json_object
+
+
+###############################################################
+################ Main #########################################
+###############################################################
+def create_dashboard(url, user_id, password, dashboard_json_object, dashboard_name, dashboard_args_dict,
+                     use_public_api):
     session = None
     if use_public_api:
         session = open_vrni_public_api_session(url, user_id, password)
@@ -199,29 +271,22 @@ def create_dashboard(url, user_id, password, dashboard_json_object, dashboard_na
         # create the dashboard
         dashboard_id = None
         if dashboard_name is None:
-            dashboard_name = dashboard_json_object.default_board_name
+            dashboard_name = substitute_arguments(dashboard_json_object.default_board_name, dashboard_args_dict)
 
         if use_public_api:
-            dashboard_id = create_pinboard_pre_69(session, dashboard_name, dashboard_json_object.description)
+            dashboard_id = create_pinboard_pre_db(session, dashboard_name,
+                                                  substitute_arguments(dashboard_json_object.description, dashboard_args_dict))
         else:
-            dashboard_id = create_pinboard_post_69(session, dashboard_name, dashboard_json_object.description)
+            dashboard_id = create_pinboard_post_db(session, dashboard_name,
+                                                   substitute_arguments(dashboard_json_object.description, dashboard_args_dict))
 
         for pin in dashboard_json_object.pins:
             if use_public_api:
-                add_pin_to_pinboard_pre_69(session, dashboard_id, pin.pin_name, pin.pin_query)
+                add_pin_to_pinboard_pre_db(session, dashboard_id, pin.pin_name,
+                                           substitute_arguments(pin.pin_query, dashboard_args_dict))
             else:
-                add_pin_to_pinboard_post_69(session, dashboard_id, pin.pin_name, pin.pin_query)
-
-def download_dashboard_file(sourceurl, destinationfile = "./temp_dashboard.json"):
-    response = requests.get(sourceurl)
-    if response.status_code == 200:
-        with open(destinationfile, 'wb') as file:
-            file.write(response.content)
-        print("Online Dashboard File downloaded successfully.")
-    else:
-        print("Failed to download the online Dashboard file.")
-        return None
-    return destinationfile
+                add_pin_to_pinboard_post_db(session, dashboard_id, pin.pin_name,
+                                            substitute_arguments(pin.pin_query, dashboard_args_dict))
 
 
 if __name__ == '__main__':
@@ -250,6 +315,10 @@ if __name__ == '__main__':
                       dest="dashboard_name",
                       help="[Optional] Name to be used for the dashboard")
 
+    parser.add_option("-a", "--args",
+                      dest="dashboard_arguments",
+                      help="[Optional] Additional arguments to be passed for dashboard creation. Syntax: -a \"arg1=val1, arg2=val2\"")
+
     (options, args) = parser.parse_args()
 
     if options.server is None or options.uid is None or options.password is None:
@@ -268,6 +337,7 @@ if __name__ == '__main__':
     password = options.password
     dashboard_name = options.dashboard_name
     dashboard_file = options.dashboard_file
+    dashboard_arguments = options.dashboard_arguments
 
     # Find which vRNI version we are dealing with here
     vrni_version = find_vrni_version(url, user_id, password)
@@ -287,4 +357,9 @@ if __name__ == '__main__':
         print("Dashboard json is invalid, cant create Dashboard")
         sys.exit(1)
 
-    create_dashboard(url, user_id, password, dashboard_json_object, dashboard_name, use_public_api)
+    dashboard_args_dict = build_dashboard_args_dict(dashboard_arguments)
+    if not json_has_valid_args(dashboard_json_object, dashboard_args_dict):
+        print("Arguments required for the dashboard are missing")
+        sys.exit(1)
+
+    create_dashboard(url, user_id, password, dashboard_json_object, dashboard_name, dashboard_args_dict, use_public_api)
