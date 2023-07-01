a68f93eb8c226143a6678ca6cf16e9f113dd708d
Devraj Baheti <dbaheti@dbaheti4QYP9.vmware.com>
2023-06-30 18:04:32 -0700

    Changes for exporting dashboard

---
+++ export-dashboard.py
@@ -0,0 +1,331 @@
+import json
+import re
+import requests
+import sys
+import urllib3
+from optparse import OptionParser
+from types import SimpleNamespace
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+###############################################################
+################ vRNI Session Management ######################
+###############################################################
+def open_vrni_public_api_session(url, user_id, password):
+    try:
+        session = requests.Session()
+
+        session.auth = (user_id, password)
+
+        data = '{"username":"' + user_id + '","password":"' + password + '", "domain": {'
+
+        domain_type = "LOCAL"
+        domain_value = ""
+
+        if not "@local" in user_id:
+            # Looks like ad/ldap login
+            domain_type = "LDAP"
+            domain_value = user_id.split("@")[1]
+
+        data += '"domain_type" : "' + domain_type + '","value":"' + domain_value + '"}}'
+
+        # Instead of requests.get(), you'll use session.get()
+        response = session.post(url + "/api/ni/auth/token", data=data, verify=False,
+                                headers={'content-type': 'application/json', 'accept': 'application/json'})
+        # print response
+
+        if response.status_code != 200:
+            print("Failed to authenticate")
+            return
+
+        loaded_json = json.loads(response.content)
+        session.headers["Authorization"] = "NetworkInsight " + loaded_json["token"]
+        session.headers["Content-Type"] = "application/json"
+        session.headers["Accept"] = "application/json"
+        session.auth = None
+        return session
+    except requests.exceptions.ConnectionError as connection_exception:
+        print("Failed to connect to " + url)
+        print(connection_exception.message)
+    return None
+
+
+def open_vrni_private_api_session(url, user_id, password):
+    try:
+        session = requests.Session()
+
+        session.auth = (user_id, password)
+
+        # data = '{"username":"' + user_id + '","password":"' + password + '", "domain": "localdomain"}'
+
+        data = '{"username":"' + user_id + '","password":"' + password + '", "domain": '
+        domain_value = "localdomain"
+
+        if not "@local" in user_id:
+            domain_value = user_id.split("@")[1]
+
+        data += '"' + domain_value + '"' + '}'
+        print(data)
+
+        # Instead of requests.get(), you'll use session.get()
+        response = session.post(url + "/api/auth/login", data=data, verify=False,
+                                headers={'content-type': 'application/json', 'accept': 'application/json'})
+        print(response)
+        # print response
+
+        if response.status_code != 200:
+            print("Failed to authenticate")
+            return
+
+        loaded_json = json.loads(response.content)
+        print(loaded_json)
+        session.headers["x-vrni-csrf-token"] = loaded_json["csrfToken"]
+        session.headers["Content-Type"] = "application/json"
+        session.headers["Accept"] = "application/json"
+        session.auth = None
+        return session
+    except requests.exceptions.ConnectionError as connection_exception:
+        print("Failed to connect to " + url)
+        print(connection_exception.message)
+    return None
+
+
+###############################################################
+################ Pinboard and Pin creation ####################
+###############################################################
+def export_queries_json_pre_db(pinboard, dashboard_export_file):
+    jsondata = {"pins": []}
+    for pins in pinboard['pins']:
+        jsondata['pins'].append({
+            "pin_name": pins['name'],
+            "pin_description": pins['name'],
+            "pin_query": pins['query']
+        })
+    jsondata["default_board_name"] = pinboard['name']
+    jsondata["description"] = pinboard['description']
+    print("********************Dumping data*****************")
+    print(json.dumps(jsondata))
+
+    with open(dashboard_export_file, 'a', encoding='utf-8') as f:
+        json.dump(jsondata, f, ensure_ascii=False, indent=4)
+        f.write("\n\n")
+    pass
+
+pass
+
+
+def export_pinboard_pre_db(session, dashboard_name, dashboard_export_file):
+    responseGetAll = session.get(url + "/api/ni/pinboards/", verify=False, headers={'content-type':'application/json', 'accept':'application/json'});
+    print(responseGetAll)
+    if responseGetAll.status_code != 200:
+        print("Failed to get Pinboards")
+        return
+
+    data = json.loads(responseGetAll.content)
+    print(data)
+
+    for pinboard in data["results"]:
+        if (dashboard_name.lower() == "ALL".lower()):
+            print("Adding" + pinboard["name"])
+            export_queries_json_pre_db(pinboard, dashboard_export_file)
+        elif (dashboard_name.lower() == pinboard["name"].lower()):
+            print("Adding" + pinboard["name"])
+            export_queries_json_pre_db(pinboard, dashboard_export_file)
+    return None
+
+
+def export_queries_json_post_db(session, name, query, dashboard_export_file):
+    response = session.get(url+"/api/search/query?searchString="+query, verify=False, headers={'content-type':'application/json', 'accept':'application/json'})
+    print(response)
+
+    loaded_json = json.loads(response.content)
+    print(loaded_json)
+    nameAndQueryDict = dict()
+    for applets in loaded_json["applets"]:
+        default_board_name = applets["name"]
+        description = applets["description"]
+        for indApplets in applets["applets"]:
+            if (indApplets["type"] == "QUERY"):
+                nameAndQueryDict[indApplets["id"]] = indApplets["querySets"][0]["query"]
+
+
+    jsondata = {"pins": []}
+    for name, query in nameAndQueryDict.items():
+        jsondata['pins'].append({
+            "pin_name": name,
+            "pin_description": name,
+            "pin_query": query
+        })
+
+    jsondata["default_board_name"] = default_board_name
+    jsondata["description"] = description
+    print("********************Dumping data*****************")
+    print(json.dumps(jsondata))
+
+    with open(dashboard_export_file, 'a', encoding='utf-8') as f:
+        json.dump(jsondata, f, ensure_ascii=False, indent=4)
+        f.write("\n\n")
+    pass
+
+
+def export_pinboard_post_db(session, dashboard_name, dashboard_export_file):
+    responseGetAll = session.get(url + "/api/custom-dashboards/visible", verify=False, headers={'content-type':'application/json', 'accept':'application/json'});
+    print(responseGetAll)
+    if responseGetAll.status_code != 200:
+        print("Failed to get Dashboards")
+        return
+
+    data = json.loads(responseGetAll.content)
+    print(data)
+    nameAndQueryDict = dict();
+    for loaded_json in data:
+        print(loaded_json["name"])
+        print(dashboard_name)
+        if (loaded_json["name"].lower() == "Get Started".lower()):
+            continue
+        elif (dashboard_name.lower() == "ALL".lower()):
+            print("Adding" + loaded_json["name"])
+            nameAndQueryDict[loaded_json["name"]] = loaded_json["query"]
+        elif (dashboard_name.lower() == loaded_json["name"].lower()):
+            print("Adding2" + loaded_json["name"])
+            nameAndQueryDict[loaded_json["name"]] = loaded_json["query"]
+            break
+
+    for name,query in nameAndQueryDict.items():
+        export_queries_json_post_db(session, name, query, dashboard_export_file);
+    return None
+
+
+###############################################################
+################ Data & Param validation ######################
+###############################################################
+def check_options(options):
+    if options.dashboard_export_file is None:
+        print("No file specified for export")
+        return False
+    return True
+
+
+def is_valid_dashboard_json(json_object, use_public_api):
+    json_object_is_valid = False
+    try:
+        for attr in ["default_board_name", "description", "pins"]:
+            if hasattr(json_object, attr):
+                json_object_is_valid = True
+                continue
+            else:
+                print("Failed to find " + attr + " in input json")
+                json_object_is_valid = False
+                break
+
+        if not json_object_is_valid:
+            return json_object_is_valid
+
+        if len(json_object.pins) > 20 and use_public_api:
+            print(
+                "WARNING: There are more than 20 pins in this definition, but vRNI version you are using allows only 20 pins per pinboard. "
+                "This script will use only the first 20 pins from the file")
+
+        for pin in json_object.pins:
+            if not hasattr(pin, "pin_name") or not hasattr(pin, "pin_description") or not hasattr(pin, "pin_query"):
+                print("Invalid pin definition " + pin)
+                json_object_is_valid = False
+                break
+            else:
+                json_object_is_valid = True
+    except:
+        print("Failed while loading the json from " + dashboard_export_file)
+    return json_object_is_valid
+
+
+###############################################################
+################ Misc. Utils ##################################
+###############################################################
+
+
+def find_vrni_version(url, user_id, password):
+    session = open_vrni_public_api_session(url, user_id, password)
+    response = session.get(url + "/api/ni/info/version", verify=False)
+    vrni_version = None
+    if response.status_code == 200:
+        loaded_json = json.loads(response.content)
+        vrni_version = loaded_json["api_version"]
+    session.close()
+    return vrni_version
+
+
+###############################################################
+################ Main #########################################
+###############################################################
+def export_dashboard(url, user_id, password, dashboard_export_file, dashboard_name, use_public_api):
+    session = None
+    if use_public_api:
+        session = open_vrni_public_api_session(url, user_id, password)
+    else:
+        session = open_vrni_private_api_session(url, user_id, password)
+
+    if session:
+
+        if dashboard_name is None:
+            dashboard_name = "ALL"
+
+        if use_public_api:
+            export_pinboard_pre_db(session, dashboard_name, dashboard_export_file)
+        else:
+            export_pinboard_post_db(session, dashboard_name, dashboard_export_file)
+
+
+if __name__ == '__main__':
+
+    parser = OptionParser()
+    parser.add_option("-d", "--destination", dest="server",
+                      help="vRNI Server IP/fqdn")
+
+    parser.add_option("-u", "--user",
+                      dest="uid",
+                      help="vRNI User")
+
+    parser.add_option("-p", "--password",
+                      dest="password",
+                      help="vRNI User's password")
+
+    parser.add_option("-f", "--file",
+                      dest="dashboard_export_file",
+                      help="Dashboard definition file to use for exporting the dashboard")
+
+    parser.add_option("-n", "--name",
+                      dest="dashboard_name",
+                      help="[Optional] Name of the dashboard to be exported")
+
+    (options, args) = parser.parse_args()
+
+    if options.server is None or options.uid is None or options.password is None:
+        parser.print_help()
+        print("Insufficient arguments")
+        sys.exit(1)
+
+    if not check_options(options):
+        sys.exit(1)
+
+    url = "https://" + options.server
+    user_id = options.uid
+    password = options.password
+    dashboard_name = options.dashboard_name
+    dashboard_export_file = options.dashboard_export_file
+    # dashboard_arguments = options.dashboard_arguments
+
+    # Find which vRNI version we are dealing with here
+    vrni_version = find_vrni_version(url, user_id, password)
+    if vrni_version is None:
+        print("Unable to find vRNI version, terminating.")
+        sys.exit(1)
+
+    # Find which API we are supposed to use, based on vRNI version
+    [major_version, minor_version, _] = vrni_version.split(".")
+    use_public_api = False
+    if int(major_version) <= 6:
+        if int(minor_version) < 9:
+            use_public_api = True
+
+    export_dashboard(url, user_id, password, dashboard_export_file, dashboard_name, use_public_api)
